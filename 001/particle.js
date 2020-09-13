81df6486a2b63ba633b1065ecf21c9f286e0860b
TaiseiGoto <goto@aidma-hd.jp>
2020-09-13 19:01:27 +0900

    coding challenge ~22:35

--- 001/particle.js
+++ 001/particle.js
@@ -1,35 +1,79 @@
 class Particle {
     constructor() {
+        this.fov = 45;
         this.pos = createVector(width / 2, height / 2);
         this.rays = [];
-        for ( let a = 0; a < 360; a += 1) {
+        this.heading = 0;
+        this.offset = 0;
+        for ( let a = -30; a < 30; a += 1) {
             this.rays.push(new Ray(this.pos, radians(a)));
         }
+    }
+
+    updateFOV(fov) {
+        this.fov = fov;
+        this.rays = [];
+        for (let a = -this.fov / 2; a < this.fov / 2; a += 1) {
+            this.rays.push(new Ray(this.pos, radians(a)));
+        }
+    }
+
+    rotate(angle) {
+        this.heading += angle;
+        let index = 0;
+        for (let a = -this.fov / 2; a < this.fov / 2; a += 1) {
+            this.rays[index].setAngle(radians(a) + this.heading);
+            index++;
+        }
+    }
+
+    move(amt) {
+        const vel = p5.Vector.fromAngle(this.heading);
+        vel.setMag(amt);
+        this.pos.add(vel);
     }
 
     update(x,y) {
         this.pos.set(x, y);
     }
 
-    look(wall) {
-        for (let ray of this.rays) {
+    //光が壁を指す時
+    look(walls) {
+        const scene = [];
+        //光線は多数出ているため、各光線毎に数値を求める
+        for (let i = 0; i < this.rays.length; i++) {
+            //複数個ある中の1つの光線を取得
+            const ray = this.rays[i];
+            //最も近い交点を取得する変数(初期値NULL)
             let closest = null;
+            //その点までの距離を記録する変数(初期値無限)
             let record = Infinity;
+            //壁が複数個生成されているため、1つずつ計算
             for (let wall of walls) {
+                //光線1つあたりに対する壁との交点を取得
                 const pt = ray.cast(wall);
+                //交点が存在する時のみ実行
                 if (pt) {
+                    //光と交点との距離を取得
                     const d = p5.Vector.dist(this.pos, pt);
+                    //光と交点との距離は、必ずrecordより小さくなるため
                     if (d < record) {
+                    //必然的にrecordには取得した光と交点との距離が代入され、
                         record = d;
+                    //一番近い交点は、必然的に取得したptとなる
                         closest = pt;
                     }
                 }
             }
+            //一番近い交点が取得できた場合(ptが存在する場合)に、光から光線を出すための座標を取得
             if (closest) {
                 stroke(255, 100);
-                line(this.pos.x, this.pos.y, closest.x, closest.y)
+                line(this.pos.x, this.pos.y, closest.x, closest.y);
             }
+            //光と交点との距離を記録するため、配列に格納
+            scene[i] = record;
         }
+        return scene;
     }
 
     show() {
--- 001/sketch.js
+++ 001/sketch.js
@@ -3,34 +3,82 @@ let walls = [];
 //光線を定義
 //位置と方向を持つ
 let ray;
-
 let particle;
+let xoff = 0;
+let yoff = 10000;
+
+const sceneW = 400;
+const sceneH = 400;
+let sliderFOV;
+const scene = [];
 
 function setup() {
-    createCanvas(1000,800);
+    createCanvas(800,400);
     //壁となる境界線を複数生成
-    for (let i = 0; i < 10; i++){
-        let x1 = random(width);
-        let x2 = random(width);
-        let y1 = random(height);
-        let y2 = random(height);
+    for (let i = 0; i < 5; i++){
+        let x1 = random(sceneW);
+        let x2 = random(sceneW);
+        let y1 = random(sceneH);
+        let y2 = random(sceneH);
         walls[i] = new Boundary(x1, x2, y1, y2);
     }
-
+    //生成したキャンバス要素の枠全てにboundaryを生成
     walls.push(new Boundary(0, 0, width, 0));
-    walls.push(new Boundary(width, 0, width, height));
-    walls.push(new Boundary(width, height, 0, height));
-    walls.push(new Boundary(0, height, 0, 0));
-
+    walls.push(new Boundary(sceneW, 0, sceneW, sceneH));
+    walls.push(new Boundary(sceneW, sceneH, 0, sceneH));
+    walls.push(new Boundary(0, sceneH, 0, 0));
+    //particleを生成
     particle = new Particle();
+    sliderFOV = createSlider(0, 360, 45);
+    sliderFOV.input(changeFOV);
 }
 
+function changeFOV() {
+    const fov = sliderFOV.value();
+    particle.updateFOV(fov);
+}
 function draw() {
+
+    if (keyIsDown(LEFT_ARROW)) {
+        particle.rotate(-0.1);
+    } else if (keyIsDown(RIGHT_ARROW)) {
+        particle.rotate(0.1);
+    } else if (keyIsDown(UP_ARROW)) {
+        particle.move(2);
+    } else if (keyIsDown(DOWN_ARROW)) {
+        particle.move(-2);
+    }
+
     background(0);
+    //壁が複数生成されているため、全てを表示する
     for (let wall of walls){
         wall.show();
     }
-    particle.update(mouseX, mouseY);
+    //マウスは動的に動き続けるため、1回の描画毎に座標を求め直す
+    // particle.update(mouseX, mouseY);
+    // particle.update(noise(xoff) * sceneW, noise(yoff) * sceneH);
+    //座標が求め直されたら表示する
     particle.show();
-    particle.look(walls);
+    // xoff += 0.01;
+    // yoff += 0,01;
+
+    //光と壁との距離を取得
+    const scene = particle.look(walls);
+    //sceneW(初期値400)/光線が壁と交わっている数分(sceneは交点が存在しないと生成されない)
+    const w = sceneW / scene.length;
+    push();
+    translate(sceneW, 0);
+    //3dの影描写を縦割りの長方形で表す
+    //光線が壁と交わっている分の長方形を生成
+    for (let i = 0; i < scene.length; i++) {
+        noStroke();
+        const sq = scene[i] * scene[i];
+        const wSq = sceneW * sceneW;
+        const b = map(sq, 0, wSq, 255, 0);
+        const h = map(scene[i], 0, sceneW, sceneH, 0);
+        fill(b);
+        rectMode(CENTER);
+        rect(i * w + w / 2, sceneH / 2, w + 1, h);
+    }
+    pop();
 }
